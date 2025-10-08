fe75677997e4667713dcca1d43721b2a3ea42ec7
jbock <jbock-java@gmx.de>
2025-10-08 14:29:02 +0200

    dynamic file size

--- group/group.asm
+++ group/group.asm
@@ -1,25 +1,19 @@
 section .data
-	st_size:		dd 0 
-	fh:			dd 0 
-	buffer:			dd 0 
-	file:			dd "data.txt"
+	siz:			dq 0
+	fh:			dq 0
+	buffer:			dq 0
+	file:			db "data.txt"
 section .bss
-	st:			resb 1024
+	st:			resq 1024 ; > sizeof(struct stat)
 section .text
 
 global main
-
-do_brk:
-        mov	rax, 12		;brk
-        mov	rdi, 0
-	syscall
-	ret
 
 do_mmap:
         mov	rax, 9		;mmap
         mov	rdi, 0		;addr
         mov	rdx, 3		;prot r=1 w=2
-        mov	rsi, [st + 48] 	;len
+        mov	rsi, [siz] 	;len
         mov	r8, -1		;fd
         mov	r9, 0		;offset
         mov	r10, 34		;flags map_private=0x02, map_anonymous=0x20
@@ -71,23 +65,22 @@ main:
 	mov	rdi, [fh]	;copy fh
 	mov	rsi, st		;into st
 	syscall
+	mov	rdi, [st + 48]	;store size
+	mov	[siz], rdi	;store size
 
-	call	do_brk
-	call	do_brk
-	call	do_brk
 	call	do_mmap
 	mov	[buffer], rax	;store buffer
 
 	mov	rax, 0		;read
 	mov	rdi, [fh]	;from fh
 	mov	rsi, buffer	;into buffer
-	mov	rdx, st + 48 	;size
+	mov	rdx, [siz] 	;size
 	syscall
 
 	mov	rax, 1		;write
 	mov	rdi, 1		;stdout
 	mov	rsi, buffer	;from buffer
-	mov	rdx, st + 48	;offset of st_size
+	mov	rdx, [siz]	;offset of st_size
 	syscall
 
 	mov	rax, 3		;close
@@ -96,7 +89,7 @@ main:
 
 	mov	rax, 11		;munmap
 	mov	rdi, buffer	;buffer
-        mov	rsi, [st + 48] 	;len
+        mov	rsi, [siz] 	;len
 	syscall
 
 	call	exit
