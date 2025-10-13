7d228d2c47b10f402d4c7ac1bb6a4f29e12b19ee
jbock <jbock-java@gmx.de>
2025-10-13 22:34:54 +0200

    draft print 0x41

--- draft/draft.c
+++ draft/draft.c
@@ -2,12 +2,21 @@
 #include <sys/stat.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 
 int fh = 0;
+int i = 0;
 
 int main(int argc, char *argv[])
 {
-	fh = 3;
-	printf("%d\n", fh);
+	char out[5];
+	out[i++] = '0';
+	out[i++] = 'x';
+	out[i++] = '4';
+	out[i++] = '1';
+	out[i++] = '\n';
+	syscall(__NR_write, 1, out, 5);
 	return 0;
 }
