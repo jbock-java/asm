f57cdc09bbc2f712502783a29594626d8ff4d748
h908714124 <jbock-java@gmx.de>
2025-10-14 16:30:41 +0200

    print_int: avoid stack push for 0, x and newline

--- print_int/print_int.s
+++ print_int/print_int.s
@@ -1,26 +1,13 @@
 #https://www.youtube.com/watch?v=_hbZN4khAyU
 #https://github.com/xmdi/SCHIZONE
-	.equ	READ, 0
 	.equ	STDOUT, 1
-	.equ	CLOSE, 3
 	.equ	WRITE, 1
-	.equ	OPEN, 2
-	.equ	FSTAT, 5
 	.equ	EXIT, 60
-	.equ	MMAP, 9
-	.equ	MUNMAP, 11
-	.equ	OFFSET_SIZE, 48
-	.equ	CHAR_O, 0x4f
-	.equ	CHAR_U, 0x55
-	.equ	CHAR_C, 0x43
-	.equ	CHAR_H, 0x48
-	.equ	BANG, 0x21
-	.equ	NEWLINE, 0xa
 
 	.data
 
 	.text
-	.globl _start
+	.globl main
 
 # char *rsi, int rdx
 write_string:
@@ -38,8 +25,6 @@ print_int:
 	push	%rcx
 	mov	%rsp, %rbp 	# save stack pointer
 	mov	$0, %rcx
-	push	$0xa		# "\n"
-	inc	%rcx
 print_int_push_loop:
 	mov	%rsi, %rax
 	and	$15, %rax
@@ -53,21 +38,21 @@ print_int_after_adjust:
 	shr	$4, %rsi
 	test	%rsi, %rsi
 	jnz	print_int_push_loop
-	push	$0x78		# "x"
-	inc	%rcx
-	push	$0x30		# "0"
-	inc	%rcx
 	mov	$0, %rdx
+	movb	$0x30, -128(%rbp, %rdx)
+	inc	%rdx
+	movb	$0x78, -128(%rbp, %rdx)
+	inc	%rdx
 print_int_pop_loop:		# copy chars from stack
 	pop	%rax
 	movb	%al, -128(%rbp, %rdx)
 	inc	%rdx
 	cmp	%rsp, %rbp
 	jne	print_int_pop_loop
+	movb	$0xa, -128(%rbp, %rdx)
+	inc	%rdx
 	lea	-128(%rbp), %rsi
-	mov	%rcx, %rdx
 	call	write_string
-
 	mov	%rbp, %rsp 	# restore stack pointer
 	pop	%rcx
 	pop	%rdx
@@ -76,12 +61,14 @@ print_int_pop_loop:		# copy chars from stack
 	pop	%rax
 	ret
 
-_start:
-	mov	$0xdeadbeef, %rsi
+main:
+	mov	$0xdead, %rsi
+	shl	$16, %rsi
+	add	$0xbeef, %rsi
 	call	print_int
-	jmp	_exit
+	jmp	exit
 
-_exit:
+exit:
 	movq	$EXIT, %rax
 	movq	$0, %rdi
 	syscall
